88469dc9cd0b00c6d1606d47f2b7a1de5dbe6b27
apoticaire2020 <andalossi2008@gmail.com>
2024-10-21 11:55:46 +0100

    first commit

---
+++ src/test/java/com/example/serveur_gestion_stock_produits/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveur_gestion_stock_produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
